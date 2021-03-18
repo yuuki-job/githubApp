f1ed295a8af89eae965b31e6b09d304645d814b6
yuuki <ty0427s8145460@icloud.com>
2021-03-18 23:03:09 +0900

    githubApiを使い、自分のリポジトリを取得した

--- githubApp/githubApp/ViewController.swift
+++ githubApp/githubApp/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpCell()
+        fetchData()
+        
     }
+    func setUpCell() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    func fetchData() {
+        let url = "https://api.github.com/users/yuuki-job/repos"
+        
+        AF.request(url, method: .get, parameters: nil).responseJSON { (response) in
+            
+            switch response.result{
+            
+            case .success(_):
+                let json = try? JSON(data: response.data!)
+                print(json)
+        
+            case .failure(_):
+                print("error")
+            }
+        }
+    }
+}
 
-
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
 }
 
